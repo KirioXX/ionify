03a919f21d7b518b69fcf8d3a4ed4cf58c049632
Ben Wunderlich <narujo@freenet.de>
2014-05-23 10:45:31 +0200

    Menü und default Icons eingefügt

--- src/com/example/IONify/MyActivity.java
+++ src/com/example/IONify/MyActivity.java
@@ -2,6 +2,8 @@ package com.example.IONify;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +13,12 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
